9166fbf6f8832c28cc74db7db3618b0287bbbe46
Carlos Rejano <carlos.rejano@adevinta.com>
2023-09-06 17:32:14 +0200

    Feature to SNAT VPC CIDRs with node IP

--- utils/utils.go
+++ utils/utils.go
@@ -42,3 +42,18 @@ func GetEnv(env, defaultVal string) string {
 	}
 	return defaultVal
 }
+
+// Returns de difference between two string slices
+func DifferenceBetweensStringSlices(a, b []string) []string {
+	mb := make(map[string]struct{}, len(b))
+	for _, x := range b {
+		mb[x] = struct{}{}
+	}
+	var diff []string
+	for _, x := range a {
+		if _, found := mb[x]; !found {
+			diff = append(diff, x)
+		}
+	}
+	return diff
+}
